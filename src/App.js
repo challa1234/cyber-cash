e10f509a0d0cc12fbb3582c563f1c5430857b92d
kiran challa <146921900+challa1234@users.noreply.github.com>
2023-10-10 15:45:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CryptoList from './components/CryptoList';
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <CryptoList />
     </div>
   );
 }
