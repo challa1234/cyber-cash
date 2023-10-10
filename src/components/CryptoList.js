53f65b2ff3973d622375ddf570c3b7fcbc9ea67e
kiran challa <146921900+challa1234@users.noreply.github.com>
2023-10-10 15:45:15 +0530

    Create CryptoList.js

---
+++ src/components/CryptoList.js
@@ -0,0 +1,42 @@
+// src/components/CryptoList.js
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function CryptoList() {
+  const [cryptoData, setCryptoData] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets', {
+      params: {
+        vs_currency: 'usd',
+        order: 'market_cap_desc',
+        per_page: 10,
+        page: 1,
+        sparkline: false,
+      },
+    })
+    .then((response) => {
+      setCryptoData(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>Cryptocurrency Information</h1>
+      <ul>
+        {cryptoData.map((crypto) => (
+          <li key={crypto.id}>
+            <h3>{crypto.name}</h3>
+            <p>Symbol: {crypto.symbol}</p>
+            <p>Price: ${crypto.current_price}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default CryptoList;
